8939b42bac2de5781145f7ed35173134aa263a42
Sebastian Hultstrand <sebastian@storm-net.se>
2016-10-16 17:21:10 +0200

    Bugfix Added formatting for the data to the view.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,10 +3,16 @@
 namespace ProductSearcher\Controller;
 
 use Mustache_Engine;
+use ProductSearcher\Model\Product;
 use ProductSearcher\Model\ProductsDataMapper;
 
 class MainController {
 	public function mainViewAction(Mustache_Engine $engine, ProductsDataMapper $productsDataMapper){
-		return $engine->render('main', array('products' => $productsDataMapper->findAll()));
+		$products = array();
+		/** @var Product $product */
+		foreach($productsDataMapper->findAll() as $product){
+			$products[] = $product->jsonSerialize();
+		}
+		return $engine->render('main', array('products' => $products));
 	}
 }
